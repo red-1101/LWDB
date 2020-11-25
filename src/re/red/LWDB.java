6e03821430a87735edead16082c454a2e1797488
red-1101 <robertantosica@gmail.com>
2020-11-25 18:33:54 +0200

    Added addTable# to this class.

--- src/re/red/LWDB.java
+++ src/re/red/LWDB.java
@@ -1,8 +1,13 @@
 package re.red;
 
 import re.red.connectors.ConnectionHandler;
+import re.red.datatypes.DataType;
 import re.red.exceptions.TableNotFoundException;
 import re.red.manager.table.Table;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
 
 public class LWDB {
 
@@ -11,6 +16,40 @@ public class LWDB {
     public LWDB(ConnectionHandler handler){
 
         this.handler = handler;
+
+    }
+
+    public void createTable(String tableName, List<String> rowNames, List<DataType> types, List<Integer> sizes){
+
+        StringBuilder builder = new StringBuilder();
+
+        for(int i = 0; i < rowNames.size(); i++){
+
+            builder.append(rowNames.get(i)).append(" ").append(types.get(i).getSqlType()).append("(").append(sizes.get(i))
+                    .append("),");
+
+        }
+
+        int index = builder.lastIndexOf(",");
+        builder.setCharAt(index, Character.MIN_VALUE);
+
+        String query = builder.toString().trim();
+
+        System.out.println(query);
+
+        try{
+
+            PreparedStatement preparedStatement = handler.getConnection().prepareStatement(
+                    "CREATE TABLE " + tableName + " (" + query + ")"
+            );
+
+            preparedStatement.executeUpdate();
+
+        }catch (SQLException exception){
+
+            exception.printStackTrace();
+
+        }
 
     }
 
