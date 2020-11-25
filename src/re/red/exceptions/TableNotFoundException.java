f38807cd33deba6cc5d08e5a43930daa8c366b16
red-1101 <robertantosica@gmail.com>
2020-11-25 23:48:10 +0200

    Thrown when the specific table is not found inside the database.

--- src/re/red/exceptions/TableNotFoundException.java
+++ src/re/red/exceptions/TableNotFoundException.java
@@ -1,6 +1,6 @@
 package re.red.exceptions;
 
-public class TableNotFoundException extends Exception {
+public class TableNotFoundException extends LWDBException {
 
     public TableNotFoundException(String message){
 
