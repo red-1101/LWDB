82bdfedfe6df07bc2f4720525d853d14bb6043c4
red-1101 <robertantosica@gmail.com>
2020-11-25 23:48:24 +0200

    Throwing the exceptions

--- src/re/red/manager/table/TableUtil.java
+++ src/re/red/manager/table/TableUtil.java
@@ -1,5 +1,8 @@
 package re.red.manager.table;
 
+import re.red.exceptions.NotConnectedException;
+import re.red.exceptions.TableNotFoundException;
+
 import java.util.List;
 
 public interface TableUtil {
@@ -10,7 +13,7 @@ public interface TableUtil {
      * @param values The values that will be inserted into the rows accordingly. values Index 0 will be inserted into rows Index 0 and so on.
      */
 
-    void insertInto(List<String> rowNames, List<Object> values);
+    void insertInto(List<String> rowNames, List<Object> values) throws TableNotFoundException, NotConnectedException;
 
     /***
      *
@@ -18,6 +21,12 @@ public interface TableUtil {
      * @param values The value that is going to be updated to.
      */
 
-    void update(List<String> column, List<Object> values, String whereCheck, String whereTo);
+    void update(List<String> column, List<Object> values, String whereCheck, String whereTo) throws TableNotFoundException;
+
+    /***
+     *
+     * @return Returns whether the table exists in the database.
+     */
+    boolean exists();
 
 }
