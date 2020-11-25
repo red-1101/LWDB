2c80a410705da3c5f2a9724d49d86c0f34eff7e3
red-1101 <robertantosica@gmail.com>
2020-11-25 18:34:38 +0200

    Removed addTable# from this interface.

--- src/re/red/manager/table/TableUtil.java
+++ src/re/red/manager/table/TableUtil.java
@@ -1,13 +1,9 @@
 package re.red.manager.table;
 
-import re.red.datatypes.DataType;
-
 import java.util.List;
 
 public interface TableUtil {
 
     void insertInto(List<String> rowNames, List<Object> values);
-
-    void createTable(List<String> rowNames, List<DataType> types);
 
 }
